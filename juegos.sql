67b1912c783dfcd46ce27693f2e9ef1a8a39cfea
Bryanjara <brian0lo5791@gmail.com>
2022-07-01 16:45:04 -0400

    Update juegos.sql

--- juegos.sql
+++ juegos.sql
@@ -1,22 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-07-2022 a las 16:43:48
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `juegos`
@@ -561,6 +548,3 @@ ALTER TABLE `jugador`
   ADD CONSTRAINT `fk_jugador_usuario` FOREIGN KEY (`usuario_ID_usuario`) REFERENCES `usuario` (`ID_usuario`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
